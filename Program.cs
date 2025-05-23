3b3e3a3d95e5b68d24614cb88408dfe71c4bbf9f
Cilla <ceciliawannberg@gmail.com>
2025-05-23 16:07:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+
+namespace FlowControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueMenu = true;
+            do
+            {
+                Console.WriteLine("Välkommen!");
+                Console.WriteLine("För att navigera i programmet kommer du att ange siffror");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "0": continueMenu = false;
+                        Console.WriteLine("Stänger ner");
+                        break;
+                    default:
+                        Console.WriteLine("Felaktig input");
+                        break;
+
+                }
+            } while (continueMenu); 
+        }
+    }
+}
